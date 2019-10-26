3b185b07d98b8e3aabb363ea81e1eaad2b536baa
Wiliam64 <lemasson.wiliam@gmail.com>
2019-10-26 17:10:31 +0200

    MàJ

---
+++ JavaScript/Exos 0/manège.js
@@ -0,0 +1,13 @@
+let tickets = ("")
+do{
+    tickets=prompt('Combien de tours ?');}
+while(isNaN(tickets));
+
+
+
+do{
+for(let tour = 0; tour <11 || tour === 11; tour ++)
+if (tour === 0) console.log ('Le manège démarre')
+else if (tour === 11) console.log ("Le manège s'arrete")
+else console.log("C'est le tour numéro : " + tour)}
+while(tickets)
