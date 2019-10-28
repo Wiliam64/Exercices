f7c219469e4b5f1afbe450ede88b9f4c208b6db2
Wiliam64 <lemasson.wiliam@gmail.com>
2019-10-28 12:12:34 +0100

    MàJ

--- JavaScript/Bases/0-Débuter/Manège/manège.js
+++ JavaScript/Bases/0-Débuter/Manège/manège.js
@@ -5,9 +5,8 @@ while(isNaN(tickets));
 
 
 
-do{
+
 for(let tour = 0; tour <11 || tour === 11; tour ++)
 if (tour === 0) console.log ('Le manège démarre')
 else if (tour === 11) console.log ("Le manège s'arrete")
-else console.log("C'est le tour numéro : " + tour)}
-while(tickets)
+else console.log("C'est le tour numéro : " + tour)
