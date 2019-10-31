0e03f293e2ccb561f83252e063eb750cf4dc6223
Wiliam64 <lemasson.wiliam@gmail.com>
2019-10-31 15:08:05 +0100

    MàJ

--- JavaScript/Bases/0-Débuter/Devine le nombre/devine.js
+++ JavaScript/Bases/0-Débuter/Devine le nombre/devine.js
@@ -2,7 +2,7 @@ console.log("Bienvenue dans ce jeu de devinette !");
 
 let solution = Math.floor(Math.random() * 100) + 1; 
 
-for (let i = 0; i <= 6; i++) { 
+for (let i = 0; i <= 7; i++) { 
     let essai = Number(prompt("Entrez un nombre entre 1 et 100 pour deviner le nombre aléatoire."));
     if (essai === solution) {
         console.log("Vous avez trouvé, bravo !");
@@ -14,6 +14,6 @@ for (let i = 0; i <= 6; i++) {
             console.log(essai + " est trop grand");
     }
 
-if (i === 6){
+if (i === 7){
     console.log("Vous avez perdu ! Le nombre était : " + solution);
 }}
