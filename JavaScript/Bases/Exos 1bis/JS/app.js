bef0f126327612e6d03bae6f794b92be3c76d7c8
Wiliam64 <lemasson.wiliam@gmail.com>
2019-11-28 10:26:10 +0100

    Maj

---
+++ JavaScript/Bases/Exos 1bis/JS/app.js
@@ -0,0 +1,12 @@
+let deg = 0;
+let anim = function () {
+    deg ++;
+    this.style.transform = "rotate(" + 360*deg + "deg) ";
+    this.style.transition ="linear";
+    this.style.transitionDuration = "0.5s";
+    this.style.elevateZoom()
+}
+    let monImage = new Image(1500, 900);
+    document.body.appendChild(monImage);
+    monImage.src = "https://cdn.pixabay.com/photo/2014/10/16/09/15/lens-490806_960_720.jpg";
+    monImage.addEventListener("click", anim);
