d17146e6bb85d3d69c732106f1109a432fa47cc7
Wiliam64 <lemasson.wiliam@gmail.com>
2019-12-11 11:05:12 +0100

    maj

--- JavaScript/Algorithmes/Entrainement/javascript.js
+++ JavaScript/Algorithmes/Entrainement/javascript.js
@@ -1,11 +1,21 @@
 let id = ("12345");
 let iduser = ("");
+let mdp = ("123")
+let mdpuser = ("")
 let essai = 0;
+console.log("Coucou");
 
-do{
-    essai++;
-    console.log("essai n°" + essai);
-    prompt("Id ?")
-    
-}
-while(iduser != id)
+    do{
+        iduser = prompt("Identifiants");
+        essai++;
+        if(essai == 6){break};
+        if(iduser == id){
+            do {
+                essai = 0
+                mdpuser = prompt('Mot de passe');
+                essai++;
+                if(essai == 3){break}
+            }
+            while(mdpuser !== mdp)
+    }}
+    while(iduser !== id);
