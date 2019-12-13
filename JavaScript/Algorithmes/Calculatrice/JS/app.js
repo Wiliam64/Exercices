fba408624581fe008f1268280ac3d9f24683c38f
Wiliam64 <lemasson.wiliam@gmail.com>
2019-12-13 13:36:25 +0100

    MaJ

--- JavaScript/Algorithmes/Calculatrice/JS/app.js
+++ JavaScript/Algorithmes/Calculatrice/JS/app.js
@@ -1,20 +1,32 @@
 let chiffre = document.querySelectorAll('.caractere');
 let n1 = "";
+let n2 =""
 let egal = document.getElementById('egale');
-let clean = document.getElementById('clean')
+let clean = document.getElementById('clean');
+let ecran = document.getElementById('ecran');
+
+ecran.innerText = ""
 
 chiffre.forEach(element => {
     element.addEventListener('click', () => {
         n1 += element.textContent;
         console.log(n1);
+        ecran.textContent += element.textContent;
     }) 
 });
 
 egal.addEventListener('click', () => {
-    n1 = eval(n1)
+    n1 = eval(n1);
+    ecran.textContent = eval(ecran.textContent)
     console.log("Le résultat est : ", n1);
 })
 clean.addEventListener('click', () => {
     n1 = "";
-    console.clear()
+    console.clear();
+    ecran.textContent = ""
+})
+
+document.addEventListener('keydown', (e) => {
+    ecran.textContent += e.key
+    console.log(n2);
 })
