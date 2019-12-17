dc80bb95ae49da717d3c49f2aa5b5ea1b8c288bf
Wiliam64 <lemasson.wiliam@gmail.com>
2019-12-17 14:20:51 +0100

    maj

--- JavaScript/Ajax/Fromage/JS/script.js
+++ JavaScript/Ajax/Fromage/JS/script.js
@@ -1,39 +1,19 @@
-//Décommenter pour voir le résultat 
-//On crée un objet XMLHttpRequest
-let xhr = new XMLHttpRequest();
+let requete = new XMLHttpRequest;
 
-//On initialise notre requête avec open()
-xhr.open("GET", "JS/fromages.json");
+requete.open("GET", "JS/fromages.json");
 
-//On veut une réponse au format JSON
-xhr.responseType = "json";
+requete.responseType = "json"
 
-//On envoie la requête
-xhr.send();
+requete.send();
 
-//Dès que la réponse est reçue...
-xhr.onload = function(){
-    //Si le statut HTTP n'est pas 200...
-    if (xhr.status != 200){ 
-        //...On affiche le statut et le message correspondant
-        alert("Erreur " + xhr.status + " : " + xhr.statusText);
-    //Si le statut HTTP est 200, on affiche le nombre d'octets téléchargés et la réponse
-    }else{ 
-        alert(xhr.response.length + " octets  téléchargés\n" + JSON.stringify(xhr.response));
-    }
-};
-
-//Si la requête n'a pas pu aboutir...
-xhr.onerror = function(){
-    alert("La requête a échoué");
-};
-
-//Pendant le téléchargement...
-xhr.onprogress = function(event){
-    //lengthComputable = booléen; true si la requête a une length calculable
-    if (event.lengthComputable){
-        //loaded = contient le nombre d'octets téléchargés
-        //total = contient le nombre total d'octets à télécharger
-        alert(event.loaded + " octets reçus sur un total de " + event.total);
-    }
+requete.onload = function(){
+    alert("Requête aboutit")
+    console.log(JSON.stringify(requete.response))
+   let fromage = requete.response
+   console.log(fromage);
+   fromage.forEach(element => {
+       console.log(element.nom);
+       
+       
+   });
 };
