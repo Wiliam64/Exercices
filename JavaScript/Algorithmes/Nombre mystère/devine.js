0d5ddf38c83d99304239de1aa26589c79b014b30
Wiliam64 <lemasson.wiliam@gmail.com>
2019-12-17 09:46:51 +0100

    MaJ

---
+++ JavaScript/Algorithmes/Nombre mystère/devine.js
@@ -0,0 +1,21 @@
+console.log("Bienvenue dans ce jeu de devinette !");
+
+let solution = Math.floor(Math.random() * 100) + 1; 
+
+for (let i = 0; i <= 7; i++) { 
+    let essai = Number(prompt("Entrez un nombre entre 1 et 100 pour deviner le nombre aléatoire."));
+    if (essai === solution) {
+        console.log("Vous avez trouvé, bravo !");
+		alert("Vous avez trouvé en " + i + " essai(s)")
+        break; 
+    } else if ( essai < solution){ 
+            alert(essai + " est trop petit");
+    } else {
+            alert(essai + " est trop grand");
+    }
+
+if (i === 7){
+    alert("Vous avez perdu ! Le nombre était : " + solution);
+}
+
+}
---
+++ JavaScript/JQuerry/JS/app.js
@@ -0,0 +1,7 @@
+let span = document.querySelectorAll('span');
+span[0].style.color = "red";
+span[1].style.color = "grey";
+
+
+$("#selecteurJQ span:first").css('color', 'red')
+$("#selecteurJQ span:last").css('color', 'grey')
