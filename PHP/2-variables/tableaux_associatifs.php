15e10fbca59f6a4032cf43ab1c82d33576539a03
Wiliam64 <lemasson.wiliam@gmail.com>
2020-01-13 16:17:09 +0100

    MaJ

--- PHP/2-variables/tableaux_associatifs.php
+++ PHP/2-variables/tableaux_associatifs.php
@@ -9,10 +9,15 @@ var_dump($activites);
 $pierre = array("1"=>"Php","2"=>"Javascript","3"=>"Python");
 $paul = array("1"=>"Javascript","2"=>"Css","3"=>"Php");
 $jacques = array("1"=>"Java","2"=>"Php","3"=>"Javascript");
-$preferences = array_merge_recursive($pierre, $paul, $jacques);
+
+$preferences1 = array_merge($pierre, $paul, $jacques);
+$preferences2 = array('Pierre' => $pierre, 'Paul' => $paul, 'Jacques' => $jacques);
 
 
+var_dump($preferences1);
+var_dump($preferences2);
 
-var_dump($preferences);
+
+var_dump(array_count_values($preferences1));
 
 ?>
