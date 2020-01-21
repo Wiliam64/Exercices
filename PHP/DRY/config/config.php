9227583437f22b5c1c1bd6eea470150d34c3837c
Wiliam64 <lemasson.wiliam@gmail.com>
2020-01-21 15:12:04 +0100

    MaJ

--- PHP/DRY/config/config.php
+++ PHP/DRY/config/config.php
@@ -1,7 +1,7 @@
 <?php
 define("template", '/var/www/html/Exercices/PHP/DRY/template');
-define("img", 'http://localhost/Exercices/PHP/DRY/ppj/img');
-define("css", 'http://localhost/Exercices/PHP/DRY/css/style.css');
+define("img", 'http://localhost/Exercices/PHP/DRY/asset/img');
+define("css", 'http://localhost/Exercices/PHP/DRY/asset/css/style.css');
 define("fonction", "/var/www/html/Exercices/PHP/DRY/fonction");
 define("classes", "/var/www/html/Exercices/PHP/DRY/Classes");
 ?>
